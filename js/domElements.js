ce30cbf2c9d8e550b27d16bbcfc7022861651e79
Koozchma <131485358+Koozchma@users.noreply.github.com>
2025-05-08 15:39:59 -0400

    Update domElements.js

--- js/domElements.js
+++ js/domElements.js
@@ -1,28 +1,30 @@
 // js/domElements.js
 
+// Game Area
 export const capitalDisplay = document.getElementById('capitalDisplay');
 export const earnButton = document.getElementById('earnButton');
 export const wageValueDisplay = document.getElementById('wageValueDisplay');
 export const capitalFeedback = document.getElementById('capitalFeedback');
 export const interestRateDisplay = document.getElementById('interestRateDisplay');
-export const totalMaintenanceDisplay = document.getElementById('totalMaintenanceDisplay');
+export const totalCpsDisplay = document.getElementById('totalCpsDisplay'); // Factory Production
+export const totalMaintenanceDisplay = document.getElementById('totalMaintenanceDisplay'); // Needs Maintenance
 export const netGainDisplay = document.getElementById('netGainDisplay');
 export const gameTimeDisplay = document.getElementById('gameTime');
-export const currentStageDisplay = document.getElementById('currentStage');
-export const storyTextDisplay = document.getElementById('storyText');
-export const storyProgressBar = document.getElementById('storyProgress');
 
+// Status Bars
 export const healthBar = document.getElementById('healthBar');
 export const healthText = document.getElementById('healthText');
 export const hungerBar = document.getElementById('hungerBar');
 export const hungerText = document.getElementById('hungerText');
 
+// Career Section
 export const currentWageDisplay = document.getElementById('currentWageDisplay');
 export const promotionLevelDisplay = document.getElementById('promotionLevelDisplay');
 export const promoteButton = document.getElementById('promoteButton');
 export const promotionClicksDisplay = document.getElementById('promotionClicksDisplay');
 export const promotionClickProgress = document.getElementById('promotionClickProgress');
 
+// Needs Section
 export const foodLevelDisplay = document.getElementById('foodLevelDisplay');
 export const foodProductionDisplay = document.getElementById('foodProductionDisplay');
 export const foodMaintenanceDisplay = document.getElementById('foodMaintenanceDisplay');
@@ -36,6 +38,15 @@ export const shelterMaintenanceDisplay = document.getElementById('shelterMainten
 export const shelterUpgradeCostDisplay = document.getElementById('shelterUpgradeCostDisplay');
 export const upgradeShelterButton = document.getElementById('upgradeShelterButton');
 
+// Factories Section
+export const factoriesGrid = document.querySelector('.factories-grid'); // Container for factory cards
+
+// Storyline Section
+export const currentStageDisplay = document.getElementById('currentStage');
+export const storyTextDisplay = document.getElementById('storyText');
+export const storyProgressBar = document.getElementById('storyProgress');
+
+// Game Over Screen
 export const gameOverScreen = document.getElementById('gameOverScreen');
 export const gameOverTitle = document.getElementById('gameOverTitle');
 export const gameOverMessage = document.getElementById('gameOverMessage');
